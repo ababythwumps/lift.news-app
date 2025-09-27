0c1bd4e2b1e3b75a6d180ce7da943161524d4cde
ababythwumps <michaelaedan@duck.com>
2025-09-27 12:49:34 -0700

    end it

---
+++ lift.newsTests/lift_newsTests.swift
@@ -0,0 +1,16 @@
+//
+//  lift_newsTests.swift
+//  lift.newsTests
+//
+//  Created by Michael Kao on 9/27/25.
+//
+
+import Testing
+
+struct lift_newsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ lift.newsUITests/lift_newsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  lift_newsUITestsLaunchTests.swift
+//  lift.newsUITests
+//
+//  Created by Michael Kao on 9/27/25.
+//
+
+import XCTest
+
+final class lift_newsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
